45c773f1e0584169f5d95cb111d81fcda169d9b3
aidaiym <omurbekovnaaidaiym@gmail.com>
2022-07-24 23:40:27 +0600

    usage

--- dart/effective_dart/usage.dart
+++ dart/effective_dart/usage.dart
@@ -7,4 +7,19 @@ Dart has three core collection types: List, Map, and Set. The Map and Set classe
 var points = <Point>[];
 var addresses = <String, Address>{};
 var counts = <int>{};
+
+DO use a function declaration to bind a function to a name.
+Linter rule: prefer_function_declarations_over_variables
+
+Modern languages have realized how useful local nested functions and closures are. It’s common to have a function defined inside another one. In many cases, this function is used as a callback immediately and doesn’t need a name. A function expression is great for that.
+
+But, if you do need to give it a name, use a function declaration statement instead of binding a lambda to a variable.
+
+void main() {
+  void localFunction() {
+    ...
+  }
+}
+
+DON’T create a lambda when a tear-off will do.
 */
