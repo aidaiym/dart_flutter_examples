a7d8d40694fb9886190d38c6070bc1053f4c8c1c
aidaiym <omurbekovnaaidaiym@gmail.com>
2022-07-26 22:26:04 +0600

    usage

--- dart/effective_dart/usage.dart
+++ dart/effective_dart/usage.dart
@@ -22,4 +22,19 @@ void main() {
 }
 
 DON’T create a lambda when a tear-off will do.
+DO use a function declaration to bind a function to a name.
+Linter rule: prefer_function_declarations_over_variables
+
+Modern languages have realized how useful local nested functions and closures are. It’s common to have a function defined inside another one. In many cases, this function is used as a callback immediately and doesn’t need a name. A function expression is great for that.
+
+But, if you do need to give it a name, use a function declaration statement instead of binding a lambda to a variable.
+
+void main() {
+  void localFunction() {
+    ...
+  }
+}
+
+DON’T create a lambda when a tear-off will do.
+DO use = to separate a named parameter from its default value.
 */
